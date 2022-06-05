0a09d0dfec3f1ee18c5193f41d3191b7cd3cc417
Pawel Dudko <paweldudko@outlook.com>
2022-06-05 15:34:22 +0200

    solution for task1b

--- src/test/java/pl/sdacademy/unit/test/advance/exercises/task1/CalculatorTest.java
+++ src/test/java/pl/sdacademy/unit/test/advance/exercises/task1/CalculatorTest.java
@@ -1,5 +1,6 @@
 package pl.sdacademy.unit.test.advance.exercises.task1;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -7,20 +8,36 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator = new Calculator();
 
     @ParameterizedTest
     @MethodSource("dataProviderForMultiply")
     void shouldMultiplyTwoDigits(int firstDigit, int secondDigit, int expectedResult) {
         //given
-        Calculator calculator = new Calculator();
         //when
         int result = calculator.multiply(firstDigit, secondDigit);
         //then
         assertEquals(expectedResult, result); //junit
         assertThat(result).isEqualTo(expectedResult); //assertJ
+    }
+
+    @Test
+    void shouldThrowExceptionWhenSecondDigitIsEqualToZero() {
+        //given
+        int firstDigit = 5;
+        int secondDigit = 0;
+        //when & then
+        //junit
+        assertThrows(IllegalArgumentException.class,
+                () -> calculator.divide(firstDigit, secondDigit));
+        //assertJ
+        assertThatThrownBy(() ->calculator.divide(firstDigit, secondDigit))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Can't divide by 0!");
     }
 
     private static Stream<Arguments> dataProviderForMultiply() {
